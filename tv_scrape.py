87e9799edf2be0ec102b114b3a8e8af0a99f3945
Gerald Bryan <69706675+geraldbryan@users.noreply.github.com>
2024-10-23 14:42:05 +0700

    Update tv_scrape.py

--- tv_scrape.py
+++ tv_scrape.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import os 
 import time
